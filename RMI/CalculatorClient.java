b04be48e5c43db242e4a80763248666bb81bb19f
Cesar Hideki Imai <116912049+Cehiim@users.noreply.github.com>
2024-09-14 20:55:41 -0300

    ajuste no cliente

--- RMI/CalculatorClient.java
+++ RMI/CalculatorClient.java
@@ -7,19 +7,19 @@ public class CalculatorClient {
     public static void main(String[] args) {
         try {
             // Get the registry
-            Registry registry = LocateRegistry.getRegistry("localhost");
+            Registry registry = LocateRegistry.getRegistry("192.168.176.1");
 
             // Lookup the Calculator object
             Calculator stub = (Calculator) registry.lookup("Calculator");
 
             // Invoke remote methods
-            Fracao a = new Fracao(1, 2);
-            Fracao b = new Fracao(5, 7);
+            Fracao a = new Fracao(1, 2); // Fração: 1/2
+            Fracao b = new Fracao(3, 10); // Fração: 3/10
 
-            System.out.println("Addition: " + stub.addF(a, b));
-            System.out.println("Subtraction: " + stub.subtractF(a, b));
-            System.out.println("Multiplication: " + stub.multiplyF(a, b));
-            System.out.println("Division: " + stub.divideF(a, b));
+            System.out.println("Addition: " + stub.addF(a, b)); // Resultado: 8/10
+            System.out.println("Subtraction: " + stub.subtractF(a, b)); // Resultado: 2/10
+            System.out.println("Multiplication: " + stub.multiplyF(a, b)); // Resultado: 3/20
+            System.out.println("Division: " + stub.divideF(a, b)); // Resultado: 10/6
         } catch (Exception e) {
             System.err.println("Client exception: " + e.toString());
             e.printStackTrace();
