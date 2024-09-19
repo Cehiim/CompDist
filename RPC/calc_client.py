2f454dda919d285a39dc07072272d929c2c656be
Cesar Imai <cesar.imai36@gmail.com>
2024-09-18 22:13:42 -0300

    grpc feito

--- RPC/calc_client.py
+++ RPC/calc_client.py
@@ -5,10 +5,16 @@ import calc_pb2_grpc
 def run():
 	with grpc.insecure_channel('localhost:50054') as channel:
 		stub = calc_pb2_grpc.CalculatorStub(channel)
-		frac1 = Fraction(1,2)
-		frac2 = Fraction(2,3)
-		response = stub.Mult(calc_pb2.MultRequest(frac1, frac2))
-		print("Mult result:", response.result)
+		f1 = calc_pb2.Fraction(num = 1, den = 2)
+		f2 = calc_pb2.Fraction(num = 2, den = 3)
+
+		request = calc_pb2.MultRequest(frac1 = f1, frac2= f2)
+
+		response = stub.Mult(request)
+
+		num_res = response.result.num
+		den_res = response.result.den
+		print(f"Mult result: {num_res}/{den_res}")
 
 if __name__ == '__main__':
 	run()
--- RPC/numComplex.py
+++
@@ -1,4 +0,0 @@
-class NumeroComplexo:
-  def __init__(self, real, imaginario):
-    self.real = real
-    self.imaginario = imaginario
